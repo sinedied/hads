db92111eb364adae580bcf1a041e32732c33e251
mykiimike <mv@verman.fr>
2017-03-30 13:37:24 +0200

    Remove useless path.join

--- hads.js
+++ hads.js
@@ -41,10 +41,6 @@ if (args.help || args._.length > 1) {
   process.exit();
 }
 
-// Find node_modules base path
-var modulesBasePath = require.resolve('highlight.js');
-modulesBasePath = modulesBasePath.substr(0, modulesBasePath.lastIndexOf('node_modules'));
-
 var docPath = args._[0] || './';
 var rootPath = path.resolve(docPath);
 var imagesPath = path.join(rootPath, Helpers.sanitizePath(args.i));
@@ -56,12 +52,13 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/_hads/', express.static(path.join(__dirname, '/public')));
-app.use('/_hads/highlight/', express.static(path.join(modulesBasePath, 'node_modules/highlight.js/styles')));
-app.use('/_hads/octicons/', express.static(path.join(modulesBasePath, 'node_modules/octicons/build/font')));
-app.use('/_hads/ace/', express.static(path.join(modulesBasePath, 'node_modules/ace-builds/src-min/')));
-app.use('/_hads/mermaid/', express.static(path.join(modulesBasePath, 'node_modules/mermaid/dist/')));
-app.use('/_hads/dropzone/', express.static(path.join(modulesBasePath, 'node_modules/dropzone/dist/min/')));
+
+app.use('/_hads/', express.static(__dirname+'/public'));
+app.use('/_hads/highlight/', express.static(__dirname+'node_modules/highlight.js/styles'));
+app.use('/_hads/octicons/', express.static(__dirname+'node_modules/octicons/build/font'));
+app.use('/_hads/ace/', express.static(__dirname+'node_modules/ace-builds/src-min/'));
+app.use('/_hads/mermaid/', express.static(__dirname+'node_modules/mermaid/dist/'));
+app.use('/_hads/dropzone/', express.static(__dirname+'node_modules/dropzone/dist/min/'));
 
 const ROOT_FILES = ['index.md', 'README.md', 'readme.md'];
 const STYLESHEETS = ['/highlight/github.css', '/octicons/octicons.css', '/css/github.css', '/css/style.css',
